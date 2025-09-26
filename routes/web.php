b5fd43c70000377f33b8617c3f1b2a4c749c2ef2
damouu <mouadsehbaoui@gmail.com>
2025-09-26 23:39:15 +0200

    #LAP-6 Done Unassigned Fixed
    refactor web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user/{email}', [UserController::class, 'getUserByEmail']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);

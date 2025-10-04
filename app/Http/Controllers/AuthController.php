66d369e72b452257825e858e75b8dc051b087369
damouu <mouadsehbaoui@gmail.com>
2025-10-04 11:33:30 +0200

    #LAP-8 Done Unassigned Done
    refactor: AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use DateTime;
-use DateTimeZone;
 use Exception;
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
@@ -19,6 +20,7 @@ class AuthController extends Controller
      *
      * @param Request $request
      * @return JsonResponse
+     * @throws ValidationException
      */
     public function register(Request $request): JsonResponse
     {
@@ -33,7 +35,7 @@ class AuthController extends Controller
             return response()->json($validator->errors()->first(), 400);
         }
 
-        $user = User::create([
+        User::create([
             'name' => $request->name,
             'email' => $request->email,
             'avatar_img_URL' => $request->avatar_img_URL,
@@ -43,16 +45,31 @@ class AuthController extends Controller
             'updated_at' => null,
             'password' => Hash::make($request->password),
         ]);
-        try {
-            $token = Auth::login($user);
-        } catch (Exception $e) {
-            return response()->json(['error' => 'Could not create token'], 500);
+
+        $user = User::where('email', $request->email)->firstOrFail();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+
+        } else {
+            $key = 'example_key';
+            $payload = [
+                'iss' => 'http://example.org',
+                'aud' => 'http://example.com',
+                'nbf' => 1357000000,
+                'iat' => time(),
+                'exp' => time() + 3600,
+                'user_id' => $user->id,
+                'user_studentCardUUID' => $user->studentCardUUID
+            ];
+            $jwt = JWT::encode($payload, $key, 'HS256');
         }
         return response()->json([
-            'token' => $token,
-            'user' => $user,
+            'message' => true,
+            'token' => $jwt,
         ], 201);
-
     }
 
     /**
@@ -61,6 +78,7 @@ class AuthController extends Controller
      *
      * @param Request $request
      * @return JsonResponse
+     * @throws ValidationException
      */
     public function login(Request $request): JsonResponse
     {
@@ -68,27 +86,33 @@ class AuthController extends Controller
         $position = strpos($authorization, ':');
         $password = substr($authorization, $position + 1);
         $email = substr($authorization, 0, $position);
-        $user = (new User)->where("email", $email)->first();
-        try {
-            if (!Hash::check($password, $user->password)) {
-                return response()->json(['error' => 'Invalid credentials'], 401);
-            }
-        } catch (Exception $e) {
-            return response()->json(['error' => 'Invalid credentials'], 401);
-        }
-        try {
-            $token = Auth::login($user);
-            Auth::check();
-            $user->last_logged_in_at = now();
-            $user->save();
-            return response()->json([
-                'token' => $token,
-                'expires_in' => auth('api')->factory()->getTTL() * 60,
-            ]);
+        $user = User::where("email", $email)->firstOrFail();
 
-        } catch (Exception $e) {
-            return response()->json(['error' => 'Could not create token'], 500);
+        if (!Hash::check($password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
         }
+
+        $user->last_logged_in_at = now();
+        $user->save();
+        $key = 'example_key';
+        $payload = [
+            'iss' => 'http://example.org',
+            'aud' => 'http://example.com',
+            'nbf' => 1357000000,
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'user_id' => $user->id,
+            'user_studentCardUUID' => $user->studentCardUUID
+        ];
+
+        $jwt = JWT::encode($payload, $key, 'HS256');
+
+        return response()->json([
+            'message' => true,
+            'token' => $jwt,
+        ]);
     }
 
     /**
@@ -96,35 +120,23 @@ class AuthController extends Controller
      * 取得されたのJWTの期限が切られたやらを確認する為にJWTのEXP＿DATEとヨーロッパの現在日時を比べるの関数です。
      * そのJWTはまだ使えるならJWTの内に入ってるのユーザーIDをデータベースに有るかどうか確認してからtrueのリスポンすを返事する。s。
      *
-     * @return JsonResponse returns a JsonResponse with a boolean value. if the token is still unexpired returns true.
-     * @throws Exception cheks if the user's id present in the token does exist or not in the database.
+     * @param Request $request
+     * @return JsonResponse
+     * @throws Exception
      */
     public function checkJWT(Request $request): JsonResponse
     {
         $token = $request->bearerToken();
-        $tokenDecoded = base64_decode($token);
-        $rere = explode("}", $tokenDecoded);
-        $piki = explode("{", $rere[1]);
-        $lolo = explode(",", $piki[1]);
-        foreach ($lolo as $value) {
-            $position = strpos($value, ':');
-            $key = (substr($value, 0, $position));
-            $value = (substr($value, $position + 1));
-            $arrayOfValue[$key] = $value;
-        }
-        $userID = substr($arrayOfValue['"sub"'], 1, strlen($arrayOfValue['"sub"']) - 2);
-        $tokenExpiredDate = new DateTime('@' . $arrayOfValue['"exp"']);
-        $today = new DateTime();
-        $today->setTimezone(new DateTimeZone('Europe/Paris'));
-        $tokenExpiredDate->setTimezone(new DateTimeZone('Europe/Paris'));
+        $key = 'example_key';
+        $message = '';
         try {
-            if ((new User)->findOrFail($userID) && $tokenExpiredDate > $today) {
-                return response()->json(['ok' => true]);
-            } else {
-                return response()->json(['error' => 'invalid token'], 500);
+            $decoded = JWT::decode($token, new Key($key, 'HS256'));
+            if ((new User)->findOrFail($decoded->user_id)) {
+                return response()->json(['message' => true]);
             }
-        } catch (Exception $exception) {
-            return response()->json(['error' => 'invalid token'], 500);
+        } catch (ExpiredException) {
+            $message = ['message' => false];
         }
+        return response()->json($message, 400);
     }
 }

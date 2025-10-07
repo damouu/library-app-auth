e6e97af9a973b0966df0120d45db4a04bfe0161e
damouu <mouadsehbaoui@gmail.com>
2025-10-07 22:42:32 +0200

    #LAP-10 Done Unassigned Done Done
    adds delete user route

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use App\Http\Middleware\EnsureBasicIsPresent;
 use App\Http\Middleware\EnsureTokenIsPresent;
 use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])->middleware(EnsureBasicIsPresent::class);
+Route::post('/login', [AuthController::class, 'login']);
 Route::post('/getUserProfile', [AuthController::class, 'getUserProfile'])->middleware(EnsureTokenIsPresent::class);
 Route::post('/check', [AuthController::class, 'checkJWT'])->middleware(EnsureTokenIsPresent::class);
+Route::delete('/user', [AuthController::class, 'deleteUser'])->middleware(EnsureTokenIsPresent::class);

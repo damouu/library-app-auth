358b6893f180c5983dc0d5d9277b7520f5f9d32e
damouu <mouadsehbaoui@gmail.com>
2025-11-17 23:09:21 +0100

    #LAP-23 Done Unassigned Done
    refactor: AuthService.php
    
    Reviewed-by: damouu <mouadsehbaoui@gmail.com>

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,18 +3,18 @@
 namespace App\Services;
 
 use App\Models\User;
+use App\Services\Traits\HttpClientTrait;
 use DateTime;
 use DateTimeZone;
 use Exception;
-use GuzzleHttp\Exception\ClientException;
-use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Http;
 use Illuminate\Validation\ValidationException;
 use Ramsey\Uuid\Uuid;
 
 class AuthService
 {
+    use HttpClientTrait;
+
     protected mixed $baseUrl;
 
     public function __construct(protected JWTService $JWTService)
@@ -26,25 +26,21 @@ class AuthService
     /**
      * @throws ValidationException
      */
-    public function register(array $validator): string
+    public function register(array $validator): array
     {
         $emailValid = $validator["email"];
         $emailCrop = strpos($emailValid, "@");
-        $avatarImgUrl = $validator["userName"] . "+" . (substr($emailValid, 0, $emailCrop));
+        $avatarImgUrl = $validator["user_name"] . "+" . (substr($emailValid, 0, $emailCrop));
 
-        $uuid = Uuid::uuid4()->toString();
-        (new User)->create([
-            'userName' => $validator["userName"],
+        User::create([
+            'user_name' => $validator["user_name"],
             'email' => $validator["email"],
-            'avatar_img_URL' => "https://avatar.iran.liara.run/username?username=" . $avatarImgUrl,
-            'memberCardUUID' => $uuid,
-            'last_loggedIn_at' => null,
-            'deleted_at' => null,
-            'updated_at' => null,
+            'avatar_img_url' => "https://avatar.iran.liara.run/username?username=" . $avatarImgUrl,
+            'card_uuid' => Uuid::uuid4()->toString(),
             'password' => Hash::make($validator["password"]),
         ]);
 
-        $user = (new User)->where('email', $validator["email"])->firstOrFail();
+        $user = User::where('email', $validator["email"])->firstOrFail();
 
         if (!$user || !Hash::check($validator["password"], $user->password)) {
             throw ValidationException::withMessages([
@@ -53,24 +49,20 @@ class AuthService
 
         } else {
 
-            try {
-                Http::withHeaders([
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
-                ])->post("{$this->baseUrl}" . $uuid);
-            } catch (ClientException $e) {
-                var_dump($e->getResponse());
-            } catch (ConnectionException $e) {
-                echo $e->getMessage();
-            }
             $payload = [
-                'iss' => 'http://example.org',
-                'aud' => 'http://example.com',
-                'user_id' => $user->id,
-                'user_memberCardUUID' => $user->memberCardUUID
+                'iss' => 'library-app-auth',
+                'aud' => 'library-app-borrow',
+                'sub' => $user->id,
+                'user_memberCardUUID' => $user->card_uuid
             ];
 
-            return $this->JWTService->createToken($payload);
+            $token = $this->JWTService->createToken($payload);
+
+            $response = $this->makeSafeApiCall("POST", $this->baseUrl . $user->card_uuid, $token, $user->_id);
+
+            $expiresAt = time() + 3600;
+
+            return ["memberCardUUID" => $response["memberCardUUID"], "jwt" => $token, "expires_in" => 3600, 'expires_at' => date('c', $expiresAt)];
         }
     }
 
@@ -78,9 +70,9 @@ class AuthService
      * @throws ValidationException
      * @throws Exception
      */
-    public function login(string $email, string $password): string
+    public function login(string $email, string $password): array
     {
-        $user = (new User)->where("email", $email)->firstOrFail();
+        $user = User::where("email", $email)->firstOrFail();
 
         if (!Hash::check($password, $user->password)) {
             throw ValidationException::withMessages([
@@ -88,82 +80,60 @@ class AuthService
             ]);
         }
 
-        $time = new DateTime("now", new DateTimeZone("Europe/Paris"));
+        $timeInParis = new DateTime("now", new DateTimeZone("Europe/Paris"));
         date_default_timezone_set('Europe/Paris');
-        $user->last_loggedIn_at = $time;
+        $timeInUTC = $timeInParis->setTimezone(new DateTimeZone('UTC'));
+
+        $user->last_logged_in_at = $timeInUTC;
         $user->save();
 
         $payload = [
-            'iss' => 'http://example.org',
-            'aud' => 'http://example.com',
-            'user_id' => $user->id,
-            'user_memberCardUUID' => $user->memberCardUUID
+            'iss' => 'library-app-auth',
+            'aud' => 'library-app-borrow',
+            'sub' => $user->id,
+            'user_memberCardUUID' => $user->card_uuid
         ];
 
-        return $this->JWTService->createToken($payload);
+        $token = $this->JWTService->createToken($payload);
+
+        $expiresAt = time() + 3600;
+
+        return ["memberCardUUID" => $user->card_uuid, "jwt" => $token, "expires_in" => 3600, 'expires_at' => date('c', $expiresAt)];
 
     }
 
+    /**
+     */
     public function getUserProfile(string $token): array
     {
         $decoded = $this->JWTService->verifyToken($token);
 
-        $user = (new User)->findOrFail($decoded->user_id);
+        $user = User::findOrFail($decoded->sub, ['user_name', 'avatar_img_url', 'email', 'card_uuid', 'last_logged_in_at']);
 
-        try {
-            $borrowHistory = Http::withHeaders([
-                'Accept' => 'application/json',
-                'Content-Type' => 'application/json',
-            ])->get("{$this->baseUrl}" . $user->getAttribute('memberCardUUID') . '/history');
-        } catch (ClientException $e) {
-            var_dump($e->getResponse());
-        } catch (ConnectionException $e) {
-            echo $e->getMessage();
-        }
+        $response = $this->makeSafeApiCall("GET", $this->baseUrl . $user->card_uuid . '/history?sort=borrow_start_date&direction=asc', $token, $user->_id, (array)null);
 
-
-        if (!empty($borrowHistory)) {
-            $lele = json_decode($borrowHistory->getBody()->getContents(), true);
-        }
-
-        $response = array();
-        if ($decoded->user_id == $user->id) {
-            $userData = array(
-                "email" => $user->getAttribute('email'),
-                "userName" => $user->getAttribute('userName'),
-                "memberCardUUID" => $user->getAttribute('memberCardUUID'),
-                "last_loggedIn_at" => $user->getAttribute('last_loggedIn_at'),
-                "avatar_img_URL" => $user->getAttribute('avatar_img_URL'),
-                "created_at" => $user->getAttribute('created_at')->format('Y-m-d H:i:s'),
-                "borrows_history" => $lele['borrows_UUID']);
-            $response = ['message' => true, "data" => $userData];
+        if ($decoded->sub == $user->id) {
+            $response = ["user" => $user, "borrowHistory" => $response];
         }
 
         return $response;
     }
 
-    public function deleteUser(string $token): array
+    /**
+     */
+    public function deleteUser(string $token): null|int
     {
         $decoded = $this->JWTService->verifyToken($token);
 
-        $user = (new User)->findOrFail($decoded->user_id);
+        $user = User::findOrFail($decoded->sub);
 
-        if ($decoded->user_id == $user->id) {
+        $response = $this->makeSafeApiCall("delete", $this->baseUrl . $user->card_uuid, $token, $user->_id);
+
+        if ($response != 503 && is_null($response)) {
             $user->delete();
-            try {
-                Http::withHeaders([
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
-                ])->delete("{$this->baseUrl}" . $user->memberCardUUID);
-            } catch (ClientException $e) {
-                var_dump($e->getResponse());
-            } catch (ConnectionException $e) {
-                echo $e->getMessage();
-            }
-            $response = ['status' => 201, "message" => "user deleted successfully"];
-        } else {
-            $response = ['status' => 404, "message" => "user not found"];
+            return 200;
         }
+
         return $response;
     }
 

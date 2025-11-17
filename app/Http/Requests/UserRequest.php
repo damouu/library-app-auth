d01298936fb5affbb5973015254f7516b6030e0b
damouu <mouadsehbaoui@gmail.com>
2025-11-17 23:09:57 +0100

    #LAP-22 Done Unassigned Done
    refactor: UserRequest.php
    
    Reviewed-by: damouu <mouadsehbaoui@gmail.com>

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -14,7 +14,7 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'userName' => 'required|string|max:255',
+            'user_name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6|confirmed',
             'password_confirmation' => 'required|string|min:6'

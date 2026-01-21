b4257beee88090a7a3ee8c44638f0a88b56f1619
damouu <mouadsehbaoui@gmail.com>
2026-01-21 13:14:04 +0100

    Refactor: TestCase.php (#LAP-35)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     //
+
+    protected function setUp(): void
+    {
+        putenv('OTEL_SDK_DISABLED=true');
+        putenv('OTEL_PHP_DISABLED=true');
+        putenv('OTEL_TRACES_EXPORTER=none');
+
+        parent::setUp();
+    }
 }

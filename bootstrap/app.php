e92844b5057f07e5f3217259272e5754cc1f9d0c
damouu <mouadsehbaoui@gmail.com>
2026-01-21 15:34:48 +0100

    Refactor: app.php (#LAP-36)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         apiPrefix: 'api/',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->validateCsrfTokens();
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //

655e217616f4e0b69e159700833723793cc97fc8
damouu <mouadsehbaoui@gmail.com>
2026-06-29 18:15:44 +0900

    Refactor: AppServiceProvider.php (#LAP-41)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Contracts\Clock;
+use App\Contracts\PasswordVerifier;
+use App\Kafka\EventPublisher;
+use App\Kafka\KafkaEventPublisher;
+use App\Services\AuthenticationService;
+use App\Services\UtcClock;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(
+            EventPublisher::class,
+            KafkaEventPublisher::class
+        );
+
+        $this->app->singleton(
+            PasswordVerifier::class,
+            AuthenticationService::class
+        );
+
+        $this->app->singleton(
+            Clock::class,
+            UtcClock::class
+        );
     }
 
     /**

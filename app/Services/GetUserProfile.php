f9f53c232813d700e8863c5ff4ceedb4c06f94e5
damouu <mouadsehbaoui@gmail.com>
2026-06-30 18:06:22 +0900

    Refactor: GetUserProfile.php (#LAP-43)

--- app/Services/GetUserProfile.php
+++ app/Services/GetUserProfile.php
@@ -2,25 +2,16 @@
 
 namespace App\Services;
 
-use App\Contracts\PasswordVerifier;
 use App\DTO\UserProfileDTO;
-use App\Factory\JwtPayloadFactory;
-use App\Factory\UserCreatedEventFactory;
-use App\Kafka\EventPublisher;
 use App\Repository\UserRepository;
 use Throwable;
 
 class GetUserProfile
 {
     public function __construct(
-        protected JWTService              $jwtService,
-        protected TracingService          $tracingService,
-        protected EventPublisher          $eventPublisher,
-        protected UserCreatedEventFactory $userCreatedEventFactory,
-        protected JwtPayloadFactory       $jwtPayloadFactory,
-        protected UserRegistrationService $userRegistrationService,
-        protected UserRepository          $userRepository,
-        protected PasswordVerifier        $passwordVerifier,
+        private JWTService     $jwtService,
+        private TracingService $tracingService,
+        private UserRepository $userRepository,
     )
     {
     }
@@ -31,12 +22,13 @@ class GetUserProfile
     public function getUserProfile(string $token): UserProfileDTO
     {
         return $this->tracingService->trace(
-            'user-get-profile',
+            'user.get_profile',
             function () use ($token) {
                 $decoded = $this->jwtService->verifyToken($token);
                 $user = $this->userRepository->findByEmail($decoded->email);
                 return UserProfileDTO::fromModel($user);
-            });
+            },
+        );
     }
 
 }

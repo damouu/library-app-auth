272d52fc713fb3b8986e4d70e7e52a669b469827
damouu <mouadsehbaoui@gmail.com>
2026-06-30 18:07:04 +0900

    Refactor: KafkaEventPublisher.php (#LAP-43)

--- app/Kafka/KafkaEventPublisher.php
+++ app/Kafka/KafkaEventPublisher.php
@@ -6,6 +6,7 @@ use App\dto\UserCreatedEventDTO;
 use App\Services\TracingService;
 use Junges\Kafka\Facades\Kafka;
 use OpenTelemetry\API\Trace\Propagation\TraceContextPropagator;
+use Throwable;
 
 class KafkaEventPublisher implements EventPublisher
 {
@@ -16,39 +17,66 @@ class KafkaEventPublisher implements EventPublisher
     }
 
     /**
-     * @throws \Throwable
+     * @throws Throwable
      */
     public function publish(UserCreatedEventDTO $event): void
     {
         $this->tracingService->trace(
-            'kafka-publish-event',
+            'kafka.publish',
             function () use ($event) {
                 $headers = [];
+
                 TraceContextPropagator::getInstance()->inject($headers);
+
                 Kafka::publish()
                     ->onTopic('auth-create-topic')
                     ->withHeaders($headers)
                     ->withKafkaKey($event->data->memberCardUuid)
                     ->withBody($event->toArray())
                     ->send();
-            }
+            },
+            [
+                'event.uuid' => $event->metadata->eventUuid,
+                'event.type' => $event->metadata->eventType,
+                'event.source_service' => $event->metadata->sourceService,
+
+                'messaging.system' => 'kafka',
+                'messaging.destination.name' => 'auth-delete-topic',
+                'messaging.operation' => 'publish',
+                'messaging.message.id' => $event->data->memberCardUuid,
+            ]
         );
     }
 
+    /**
+     * @throws Throwable
+     */
     public function publishDelete(UserCreatedEventDTO $event): void
     {
         $this->tracingService->trace(
-            'kafka-publish-event',
+            'kafka.publish',
             function () use ($event) {
                 $headers = [];
+
                 TraceContextPropagator::getInstance()->inject($headers);
+
                 Kafka::publish()
                     ->onTopic('auth-delete-topic')
                     ->withHeaders($headers)
                     ->withKafkaKey($event->data->memberCardUuid)
                     ->withBody($event->toArray())
                     ->send();
-            }
+            },
+            [
+                'event.uuid' => $event->metadata->eventUuid,
+                'event.type' => $event->metadata->eventType,
+                'event.source_service' => $event->metadata->sourceService,
+
+                'messaging.system' => 'kafka',
+                'messaging.destination.name' => 'auth-delete-topic',
+                'messaging.operation' => 'publish',
+                'messaging.message.id' => $event->data->memberCardUuid,
+            ]
         );
     }
 }

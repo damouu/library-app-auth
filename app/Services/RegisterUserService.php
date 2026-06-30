48e29c421b58fcbb83544ea4e34373a6bb70c87c
damouu <mouadsehbaoui@gmail.com>
2026-06-30 18:07:28 +0900

    Refactor: RegisterUserService.php (#LAP-43)

--- app/Services/RegisterUserService.php
+++ app/Services/RegisterUserService.php
@@ -6,20 +6,24 @@ use App\DTO\RegisterResponseDTO;
 use App\Factory\JwtPayloadFactory;
 use App\Factory\UserCreatedEventFactory;
 use App\Kafka\EventPublisher;
+use App\Repository\UserRepository;
 use Exception;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+use Ramsey\Uuid\Uuid;
 use Throwable;
 
 class RegisterUserService
 {
 
     public function __construct(
-        protected JWTService              $jwtService,
-        protected TracingService          $tracingService,
-        protected EventPublisher          $eventPublisher,
-        protected UserCreatedEventFactory $userCreatedEventFactory,
-        protected JwtPayloadFactory       $jwtPayloadFactory,
-        protected UserRegistrationService $userRegistrationService
+        private JWTService              $jwtService,
+        private TracingService          $tracingService,
+        private EventPublisher          $eventPublisher,
+        private UserCreatedEventFactory $userCreatedEventFactory,
+        private JwtPayloadFactory       $jwtPayloadFactory,
+        private UserRepository          $userRepository,
+        private AvatarUrlGenerator      $avatarUrlGenerator,
     )
     {
     }
@@ -29,19 +33,31 @@ class RegisterUserService
      */
     public function register(array $validator): RegisterResponseDTO
     {
-        return $this->tracingService->trace(
-            'user-registration',
-            function () use ($validator) {
-                $user = $this->userRegistrationService->create($validator);
-                $event = $this->userCreatedEventFactory->fromUser($user);
-                $this->eventPublisher->publish($event);
-                $payload = $this->jwtPayloadFactory->fromUser($user);
-                $token = $this->jwtService->createToken($payload);
-                return RegisterResponseDTO::fromToken(
-                    token: $token,
-                    expiresIn: 3600,
-                );
-            }
-        );
+        return
+            $this->tracingService->trace(
+                'user.register',
+                function ($span) use ($validator) {
+
+                    $user = $this->userRepository->create([
+                        'user_name' => $validator['user_name'],
+                        'email' => $validator["email"],
+                        'avatar_img_url' => $this->avatarUrlGenerator->generate($validator['user_name']),
+                        'card_uuid' => Uuid::uuid4()->toString(),
+                        'password' => Hash::make($validator['password']),
+                    ]);
+
+                    $event = $this->userCreatedEventFactory->fromUser($user);
+                    $span->setAttribute('event.uuid', $event->metadata->eventUuid);
+                    $span->setAttribute('event.type', $event->metadata->eventType);
+                    $span->setAttribute('user.member_card.uuid', $event->data->memberCardUuid);
+                    $this->eventPublisher->publish($event);
+                    $payload = $this->jwtPayloadFactory->fromUser($user);
+                    $token = $this->jwtService->createToken($payload);
+                    return RegisterResponseDTO::fromToken(
+                        token: $token,
+                        expiresIn: 3600,
+                    );
+                }
+            );
     }
 }

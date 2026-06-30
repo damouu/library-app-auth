5ddd73cdef7403bfd1c1dcc2cc5b275a5cb70e6a
damouu <mouadsehbaoui@gmail.com>
2026-06-30 18:06:40 +0900

    Refactor: JwtPayloadFactory.php (#LAP-43)

--- app/Factory/JwtPayloadFactory.php
+++ app/Factory/JwtPayloadFactory.php
@@ -4,31 +4,23 @@ namespace App\Factory;
 
 use App\DTO\JwtPayloadDTO;
 use App\Models\User;
-use App\Services\TracingService;
 
 class JwtPayloadFactory
 {
-    public function __construct(
-        protected TracingService $tracingService,
-    )
+    public function __construct()
     {
     }
 
     public function fromUser(User $user): JwtPayloadDTO
     {
-        return $this->tracingService->trace(
-            'build-jwt-created-event',
-            function () use ($user) {
-                return new JwtPayloadDTO(
-                    issuer: 'library-app-auth',
-                    audience: 'library-app-borrow',
-                    subject: (string)$user->getKey(),
-                    memberCardUuid: $user->card_uuid,
-                    avatarImgUrl: $user->avatar_img_url,
-                    username: $user->user_name,
-                    email: $user->email,
-                );
-            }
+        return new JwtPayloadDTO(
+            issuer: 'library-app-auth',
+            audience: 'library-app-borrow',
+            subject: (string)$user->getKey(),
+            memberCardUuid: $user->card_uuid,
+            avatarImgUrl: $user->avatar_img_url,
+            username: $user->user_name,
+            email: $user->email,
         );
     }
 }

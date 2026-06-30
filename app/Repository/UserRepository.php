fa762cab80f1fade61cb7569ea545fa136df2866
damouu <mouadsehbaoui@gmail.com>
2026-06-30 18:08:18 +0900

    Refactor: UserRepository.php (#LAP-43)

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -4,21 +4,15 @@ namespace App\Repository;
 
 use App\Models\User;
 use App\Services\TracingService;
-use OpenTelemetry\API\Trace\SpanInterface;
 use Throwable;
 
 
 class UserRepository
 {
     public function __construct(
-        protected TracingService $tracingService,
+        private readonly TracingService $tracingService,
     )
     {
-    }
-
-    public function exist(string $userUuid): bool
-    {
-        return User::where('member_card_uuid', $userUuid)->exists();
     }
 
     /**
@@ -29,48 +23,41 @@ class UserRepository
     public function findByEmail(string $email): User
     {
         return $this->tracingService->trace(
-            'user-repository-find-by-email',
-            function (SpanInterface $span) use ($email) {
-                $span->setAttribute('user.email', $email);
-                $user = User::where('email', $email)->firstOrFail();
-                $span->setAttribute('user.id', (string)$user->id);
-                return $user;
-            }
+            'repository.user.findByEmail',
+            function () use ($email) {
+                return User::where('email', $email)->firstOrFail();
+            }, [
+                'db.collection' => 'users',
+            ]
         );
     }
 
 
-    public function findProfileById(string|int $id): User
-    {
-        return $this->tracingService->trace(
-            'user-repository-find-by-id',
-            function (SpanInterface $span) use ($id) {
-                $span->setAttribute('user.id', $id);
-                return User::query()
-                    ->select([
-                        'id',
-                        'user_name',
-                        'avatar_img_url',
-                        'email',
-                        'card_uuid',
-                        'last_logged_in_at'
-                    ])
-                    ->where('id', $id)
-                    ->firstOrFail();
-            });
-    }
-
-
+    /**
+     * @throws Throwable
+     */
     public function save(User $user): User
     {
         return $this->tracingService->trace(
-            'user-repository-save',
-            function (SpanInterface $span) use ($user) {
-                $span->setAttribute('user.id', (string)$user->getKey());
-                $span->setAttribute('user.email', $user->email);
+            'repository.user.create',
+            function () use ($user) {
                 $user->save();
                 return $user;
-            }
+            }, [
+                'db.collection' => 'users',
+            ]
+        );
+    }
+
+    public function create(array $attributes): User
+    {
+        return $this->tracingService->trace(
+            'repository.user.create',
+            function () use ($attributes) {
+                return User::create($attributes);
+            }, [
+                'db.collection' => 'users',
+            ]
         );
     }
 
@@ -80,12 +67,14 @@ class UserRepository
     public function delete(User $user): bool
     {
         return $this->tracingService->trace(
-            'user-repository-delete',
-            function (SpanInterface $span) use ($user) {
-                $span->setAttribute('user.id', (string)$user->getKey());
-                $span->setAttribute('user.email', $user->email);
+            'repository.user.delete',
+            function () use ($user) {
                 return $user->delete();
-            });
+            }, [
+                'db.collection' => 'users',
+                'user.id' => $user->card_uuid,
+            ]
+        );
     }
 
 }
